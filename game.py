a1624299927ce2305ab0526a5a862d8eec11a419
Makiko <trimakichan@gmail.com>
2025-08-23 12:51:29 -0700

    Complete snowman function in game.py

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
-SNOWMAN_MIN_WORD_LENGTH = 5
-SNOWMAN_MAX_WORD_LENGTH = 8
+# SNOWMAN_MIN_WORD_LENGTH = 5
+# SNOWMAN_MAX_WORD_LENGTH = 8
 SNOWMAN_MAX_WRONG_GUESSES = 7
 
 SNOWMAN_GRAPHIC = [
@@ -20,7 +20,38 @@ def snowman(snowman_word):
     If the player wins and, 
     'Sorry, you lose! The word was {snowman_word}' if the player loses
     """
-    pass
+
+    # Track which letters in the word have been guessed correctly. 
+    correct_letter_guess_statuses = build_letter_status_dict(snowman_word)
+    # Track letters the player guessed letters are not in the word. 
+    wrong_guesses_list = []
+    
+    # Keep playing until the player guesses the word correctly or runs out of tries. 
+    while (
+        len(wrong_guesses_list) < SNOWMAN_MAX_WRONG_GUESSES
+            and not is_word_guessed(snowman_word, correct_letter_guess_statuses)
+    ):
+        
+        user_input = get_letter_from_user(correct_letter_guess_statuses, wrong_guesses_list)
+
+        if user_input in snowman_word:
+            print(f"You guessed a letter! {user_input} is in the word!")
+            correct_letter_guess_statuses[user_input] = True
+        else: 
+            print(f"You guessed it wrong. The lettter, {user_input} is NOT in the word!")
+            wrong_guesses_list.append(user_input)
+            print(f"You have {SNOWMAN_MAX_WRONG_GUESSES - len(wrong_guesses_list)} tries left.")
+
+        # Print snowman, word progress and wrong guesses after each turn. 
+        print_snowman_graphic(len(wrong_guesses_list))
+        print_word_progress_string(snowman_word, correct_letter_guess_statuses)
+        print(f"Wrong guesses: {wrong_guesses_list}")
+
+    # End message.
+    if is_word_guessed(snowman_word, correct_letter_guess_statuses):
+        print("Congratulations, you win!")
+    else:
+        print(f"Sorry, you lose! The word was {snowman_word}")
 
 
 def print_snowman_graphic(wrong_guesses_count):
